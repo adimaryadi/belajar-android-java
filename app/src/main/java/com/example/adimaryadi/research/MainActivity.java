b542b86af322418d6ffe9d2c21f73db33a702be1
adimaryadi <adimaryadiinten@gmail.com>
2018-10-20 12:38:40 +0700

    write button to console

--- app/src/main/java/com/example/adimaryadi/research/MainActivity.java
+++ app/src/main/java/com/example/adimaryadi/research/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.adimaryadi.research;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button7);
+        button.setOnClickListener(new View.OnClickListener() {
+            public static final String TAG = "Membuka Kamera";
+
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "mencoba");
+
+            }
+        });
     }
+
+
 }
